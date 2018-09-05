5ad1e58350738f549f9267df9cfaeac7810a2757
zwwuu <98434813+zwwuu@users.noreply.github.com>
2018-09-05 13:03:48 -0400

    Add functionality for DownloadSteamCMD and About button

--- src/index.js
+++ src/index.js
@@ -7,17 +7,21 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const Store = require('electron-store');
 
-document.getElementById('browseButton').addEventListener("click", selectPath);
-document.getElementById("close").addEventListener("click", close);
-document.getElementById("minimize").addEventListener("click", minimize);
-document.getElementById("downloadButton").addEventListener("click", downloadMod);
-document.getElementById("openButton").addEventListener("click", openModFolder);
+document.getElementById('browseButton').addEventListener('click', selectPath);
+document.getElementById('close').addEventListener('click', close);
+document.getElementById('minimize').addEventListener('click', minimize);
+document.getElementById('downloadButton').
+	addEventListener('click', downloadMod);
+document.getElementById('openButton').addEventListener('click', openModFolder);
+document.getElementById('downloadCMDButton').
+	addEventListener('click', downloadCMD);
+document.getElementById('aboutButton').addEventListener('click', about);
 
 const store = new Store();
 
-let steamcmdPath = "";
+let steamcmdPath = '';
 
-if (store.has("path")) {
+if (store.has('path')) {
 	document.getElementById('steamcmdPath').value = store.get('path');
 	steamcmdPath = document.getElementById('steamcmdPath').value;
 }
@@ -31,7 +35,7 @@ function selectPath() {
 		return;
 	}
 	document.getElementById('steamcmdPath').value = steamcmdPath;
-	store.set("path", steamcmdPath);
+	store.set('path', steamcmdPath);
 }
 
 function close() {
@@ -43,16 +47,16 @@ function minimize() {
 }
 
 function openModFolder() {
-	shell.showItemInFolder(`${steamcmdPath}\\steamapps\\workshop\\content\\*`)
+	shell.showItemInFolder(`${steamcmdPath}\\steamapps\\workshop\\content\\*`);
 }
 
 function downloadMod() {
-	let text = document.getElementById("downloadList").value;
+	let text = document.getElementById('downloadList').value;
 	let path = document.getElementById('steamcmdPath').value;
-	if (text.length === 0 && path === "") {
+	if (text.length === 0 && path === '') {
 		return;
 	}
-	document.getElementById("downloadButton").classList.add("is-loading");
+	document.getElementById('downloadButton').classList.add('is-loading');
 	let modList = formatText(text);
 	const promises = modList.map(mod => getValidModList(mod));
 	Promise.all(promises).then((validModList) => {
@@ -62,16 +66,24 @@ function downloadMod() {
 		generateScript(validModList);
 
 		exec(`${steamcmdPath}/steamcmd +runscript script.txt`).then(() => {
-			document.getElementById("downloadList").value = "";
-			document.getElementById("downloadButton").classList.remove("is-loading");
+			document.getElementById('downloadList').value = '';
+			document.getElementById('downloadButton').classList.remove('is-loading');
 		});
 	});
 }
 
+function downloadCMD() {
+	shell.openExternal('https://developer.valvesoftware.com/wiki/SteamCMD');
+}
+
+function about() {
+	shell.openExternal('https://github.com/Flapzw/ModDownloader');
+}
+
 function generateScript(validModList) {
-	let workshop = "";
+	let workshop = '';
 	for (let i = 0; i < validModList.length; i++) {
-		workshop += `workshop_download_item ${validModList[i].gameID} ${validModList[i].modID}\n`
+		workshop += `workshop_download_item ${validModList[i].gameID} ${validModList[i].modID}\n`;
 	}
 
 	let script = `@ShutdownOnFailedCommand 1
@@ -89,7 +101,7 @@ quit`;
 
 function getValidModList(modID) {
 	const option = {
-		uri: "https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/",
+		uri: 'https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/',
 		form: {
 			itemcount: 1,
 			publishedfileids: {0: modID}
@@ -97,13 +109,14 @@ function getValidModList(modID) {
 		json: true
 	};
 
-	return new Promise(function (resolve) {
-		request.post(option, function (err, httpResponse, body) {
+	return new Promise(function(resolve) {
+		request.post(option, function(err, httpResponse, body) {
 			if (err) {
 				return;
 			}
 
-			if (body['response']['publishedfiledetails'][0].hasOwnProperty('consumer_app_id')) {
+			if (body['response']['publishedfiledetails'][0].hasOwnProperty(
+				'consumer_app_id')) {
 				let validData = {};
 				validData.gameID = body['response']['publishedfiledetails'][0]['consumer_app_id'];
 				validData.modID = modID;
@@ -114,13 +127,13 @@ function getValidModList(modID) {
 }
 
 function formatText(text) {
-	let modList = text.replace(/\n/g, " ").replace(/\s+/g, " ").trim().split(" ");
+	let modList = text.replace(/\n/g, ' ').replace(/\s+/g, ' ').trim().split(' ');
 	let downloadList = [];
 	for (let i = 0; i < modList.length; i++) {
-		let modID = modList[i].replace(/[^0-9]+/gm, "");
+		let modID = modList[i].replace(/[^0-9]+/gm, '');
 		downloadList.push(modID);
 	}
-	return downloadList.filter(mod => mod !== "").filter(function (mod, i, arr) {
+	return downloadList.filter(mod => mod !== '').filter(function(mod, i, arr) {
 		return arr.indexOf(mod) === i;
 	});
 }
